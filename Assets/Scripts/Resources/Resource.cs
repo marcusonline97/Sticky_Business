2ecc254086efc2f4ece6825683062cc4a011213a
Amy S <enechan444@gmail.com>
2020-11-12 13:51:20 +0100

    Amy's Alpha 00
    I think I got it all working so we now have a Base to work with and from.
    The Button works and that it records resource that will be added.
    Also made a script for Resources that you can add.

---
+++ Assets/Scripts/Resources/Resource.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Resources
+{
+    [CreateAssetMenu]
+    public class Resource : ScriptableObject
+    {
+        public Color color;
+        public int AmountPerClick = 1;
+
+        public int OwnedResource
+        {
+            get => PlayerPrefs.GetInt(this.name, 1);
+            set => PlayerPrefs.SetInt(this.name, value);
+        }
+        public void Produce()
+        {
+            this.OwnedResource += this.AmountPerClick;
+        }
+    }
+}
---
+++ Assets/Scripts/Resources/ResourceUISetup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Resources
+{
+    public class ResourceUISetup : MonoBehaviour
+    {
+        public Resource[] resources;
+        public ResourceUI prefab;
+        void Start()
+        {
+            foreach (var Resource in this.resources)
+            {
+                var instance = Instantiate(this.prefab, this.transform);
+                instance.SetUp(Resource);
+            }
+        }
+    }
+}
