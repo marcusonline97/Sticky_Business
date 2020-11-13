8afe3364403baf800087e1f47861cbdba7e4b650
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-11-13 14:36:25 +0100

    Created Sophie_Scene. Created Enter Beehive Button. Created Beehive menu with Exit beehive button. Created Game Manager which controls the pause and play features. Made them into prefabs.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject BeehiveEnterButton;
+    EnterExitBeehive EnterExitBeehive;
+    float[] TimeScales = { 0, 1 };
+    void Start()
+    {
+        EnterExitBeehive = BeehiveEnterButton.GetComponent<EnterExitBeehive>();
+    }
+
+    void Update()
+    {
+        PauseGame();
+    }
+
+  void PauseGame()
+    {
+        if (EnterExitBeehive.BeehiveMenuActive)
+        {
+            Time.timeScale = TimeScales[0];
+        }
+        else
+        {
+            Time.timeScale = TimeScales[1];
+
+        }
+    }
+}
