ec9042d52652911f959f78838c1b60a970b03b53
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-11-17 10:06:51 +0100

    Remove HoneyJarbutton
    The honeyjar gets automatically updated after 10 honey clicks.

--- Assets/Scripts/Purchase/BuyableUI.cs
+++ Assets/Scripts/Purchase/BuyableUI.cs
@@ -5,26 +5,12 @@ using UnityEngine;
 using UnityEngine.UI;
 public class BuyableUI : MonoBehaviour
 {
-    public Image Door;
-    Color[] colors = { Color.white, Color.grey };
-
     public Buyable buyable;
 
     // Update is called once per frame
     void Update()
     {
-        UpdateColors();
+        buyable.BuyHoneyJar();
     }
-    void UpdateColors()
-    {
-        if (buyable.CanBuyItem)
-        {
-            Door.color = colors[0];
-        }
-        else
-        {
-            Door.color = colors[1];
 
-        }
-    }
 }
