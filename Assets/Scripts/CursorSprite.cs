481c6c680db91ff57bb3f28cb94dcc7c19b36433
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-11-18 20:03:30 +0100

    Changed Cursor
    Cursor has its own sprite for idle and clicked.

---
+++ Assets/Scripts/CursorSprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSprite : MonoBehaviour
+{
+
+    public Texture2D IdleCursorArrow;
+    public Texture2D ClickedCursorArrow;
+
+    void Start()
+    {
+        Cursor.SetCursor(IdleCursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+        
+    }
+    private void Update()
+    {
+        ChangeSprite();
+
+
+    }
+    void ChangeSprite()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.SetCursor(ClickedCursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+
+        }
+        else
+        {
+            Cursor.SetCursor(IdleCursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+
+        }
+    }
+}
