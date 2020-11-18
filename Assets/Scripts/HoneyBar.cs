9bea5860d4ed5194d6cf8cc999963e2b59afee42
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-11-18 19:23:07 +0100

    Created HoneySlider
    Created slider for the honey. Now the honeyjar fills up with honey.

---
+++ Assets/Scripts/HoneyBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HoneyBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHoney(int MaxHoney)
+    {
+        slider.maxValue = MaxHoney;
+    }
+    public void SetHoney(int Honey)
+    {
+        slider.value = Honey;
+    }
+}
